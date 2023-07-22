582ff868045288bd97733b867a00b26e2446de9e
Simen A. W. Olsen <so@bjerk.io>
2023-07-22 14:19:05 +0200

    remove authentication setup

--- src/abax-client.spec.ts
+++ src/abax-client.spec.ts
@@ -1,27 +1,15 @@
-import dotenv from 'dotenv';
 import { describe, expect, it } from 'vitest';
-import { AbaxClient, Scope, authenticateWithClientCredentials } from './main';
-dotenv.config();
+import { AbaxClient } from './main';
 
 describe('abax-client', () => {
   const initialiseClient = async () => {
-    const clientId = process.env['ABAX_CLIENT_ID'] ?? 'no-client-id';
-    const clientSecret =
-      process.env['ABAX_CLIENT_SECRET'] ?? 'no-client-secret';
-
-    const authReply = await authenticateWithClientCredentials({
-      clientId,
-      clientSecret,
-      authUrl: 'https://identity.abax.cloud',
-      scopes: [Scope.OpenApi, Scope.OpenApiEquipment],
-    });
-
     return new AbaxClient({
       baseUrl: 'production',
-      apiKey: authReply.access_token,
+      apiKey: 'not-a-secret',
     });
   };
-  it('should list equipment', async () => {
+
+  it.skip('should list equipment', async () => {
     const client = await initialiseClient();
 
     const result = await client.listEquipment({ page: 1, page_size: 2 });
@@ -29,7 +17,7 @@ describe('abax-client', () => {
     expect(result).toMatchSnapshot();
   });
 
-  it('should get equipment', async () => {
+  it.skip('should get equipment', async () => {
     const client = await initialiseClient();
 
     const result = await client.getEquipment({
