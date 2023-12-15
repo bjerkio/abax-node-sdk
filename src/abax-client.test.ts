c2e093a223c4c6eea2caff5e970357c2d28839a4
Brage Sekse Aarset <brage@bjerk.io>
2023-12-15 10:25:57 +0200

    feat: add test case with 3 retries followed by success

--- src/abax-client.test.ts
+++ src/abax-client.test.ts
@@ -33,10 +33,46 @@ describe('abax-client', () => {
       .reply(() => ({
         statusCode: 429,
       }))
-      .persist();
+      .times(4);
 
     await expect(() => client.listVehicles()).rejects.toThrow(
       'Request timed out',
     );
+  });
+
+  it('should retry thrice when getting 429', async () => {
+    const client = await initialiseClient();
+
+    mockPool
+      .intercept({
+        path: '/v1/vehicles',
+        method: 'GET',
+      })
+      .reply(() => ({
+        statusCode: 429,
+      }))
+      .times(3);
+
+    mockPool
+      .intercept({
+        path: '/v1/vehicles',
+        method: 'GET',
+      })
+      .reply(() => ({
+        statusCode: 200,
+        data: {
+          page: 1,
+          page_size: 150,
+          items: [],
+        },
+      }))
+      .times(1);
+
+    const vehicles = await client.listVehicles();
+    expect(vehicles).toEqual({
+      items: [],
+      page: 1,
+      page_size: 150,
+    });
   });
 });
