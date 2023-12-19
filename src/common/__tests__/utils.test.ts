29ed058d77accb6571cea0ba00d57ed6d20f94e5
Brage Sekse Aarset <brage@bjerk.io>
2023-12-19 12:15:59 +0100

    fix: correctly calculate the start of the next minute

---
+++ src/common/__tests__/utils.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from 'vitest';
+import { startOfTheNextMinute } from '../utils';
+
+describe('utils', () => {
+  it('should get the start of the next minute correctly', () => {
+    const now = new Date('2023-01-01T16:47:36.351Z');
+    const next = startOfTheNextMinute(now);
+
+    expect(next.toISOString()).toBe('2023-01-01T16:48:00.000Z');
+  });
+});
