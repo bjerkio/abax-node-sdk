da0333f7b1e9f1f0c100f97e8b6bbc966c0314a6
timur <tigua002@osloskolen.no>
2024-02-12 14:08:49 +0100

    Fomatting

--- src/calls/__tests__/list-capabilities.test.ts
+++ src/calls/__tests__/list-capabilities.test.ts
@@ -1,30 +1,31 @@
 import { describe, expect, it } from 'vitest';
 import { initialiseClientAndMockPool } from '../../test-utils';
 
-describe("list capabilities", () => {
-    it("should list capabilitites", async () => {
-        const { client, mockPool } = initialiseClientAndMockPool();
+describe('list capabilities', () => {
+  it('should list capabilitites', async () => {
+    const { client, mockPool } = initialiseClientAndMockPool();
 
-        mockPool
-            .intercept({
-                path: '/v1/api-capabilities',
-                method: 'GET',
-            }).reply(200, {
-                "items": [
-                    {
-                        "capability": "query-equipment"
-                    },
-                    {
-                        "capability": "query-equipment-locations"
-                    },
-                    {
-                        "capability": "query-people"
-                    }
-                ]
-            }).times(1)
-        const capabilites = await client.listCapabilities()
+    mockPool
+      .intercept({
+        path: '/v1/api-capabilities',
+        method: 'GET',
+      })
+      .reply(200, {
+        items: [
+          {
+            capability: 'query-equipment',
+          },
+          {
+            capability: 'query-equipment-locations',
+          },
+          {
+            capability: 'query-people',
+          },
+        ],
+      })
+      .times(1);
+    const capabilites = await client.listCapabilities();
 
-        expect(capabilites).toMatchSnapshot()
-    })
-
-})
+    expect(capabilites).toMatchSnapshot();
+  });
+});
