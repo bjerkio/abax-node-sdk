11f96cb7f6a63376848639e92a8c577aa9850f37
Brage Sekse Aarset <brage.aarset@gmail.com>
2024-02-13 11:48:52 +0200

    fix: allow custom baseUrl in client, enable testing with custom baseUrl (#82)

--- src/test-utils.ts
+++ src/test-utils.ts
@@ -1,18 +1,31 @@
 import { type Interceptable, MockAgent, setGlobalDispatcher } from 'undici';
 import { AbaxClient } from './abax-client';
 
-export function initialiseClientAndMockPool(): {
+export function initialiseClientAndMockPool(
+  /** A unique base URL can be provided if there is a need to test against a different endpoint.
+   * The MockPool will not work when this option is provided */
+  baseUrl?: string,
+): {
   client: AbaxClient;
   mockPool: Interceptable;
 } {
   const mockAgent = new MockAgent();
-  mockAgent.disableNetConnect();
-  setGlobalDispatcher(mockAgent);
 
   const mockPool = mockAgent.get('https://api.abax.cloud');
+  setGlobalDispatcher(mockAgent);
+
+  if (baseUrl) {
+    const client = new AbaxClient({
+      baseUrl,
+      apiKey: 'kanonball!',
+    });
+
+    return { client, mockPool };
+  }
+
+  mockAgent.disableNetConnect();
 
   const client = new AbaxClient({
-    baseUrl: 'production',
     apiKey: 'kanonball!',
   });
 
